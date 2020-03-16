c76fa8f4b11ab531214d912bbe0a7d766e05882f
MarcSteven <marczhao@memoriesus.com>
2020-03-16 10:24:39 +0800

    Add method to get UITableView in UITableViewCell

--- MemoryChainKit/Sources/Extensions/UIKit/UIView+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIView+Extension.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 public extension UIView {
+    func parentView<T:UIView>(of type:T.Type) ->T? {
+        guard let view = superview else {
+            return nil
+        }
+        return (view as? T) ?? view.parentView(of: T.self)
+    }
 
 /**
     Simply zooming in of a view: set view scale to 0 and zoom to Identity on 'duration' time interval.
