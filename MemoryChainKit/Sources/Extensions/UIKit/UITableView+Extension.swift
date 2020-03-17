04ecc749d88a472d498f22dab89fed49efc4261f
MarcSteven <marczhao@memoriesus.com>
2020-03-17 18:40:16 +0800

    [Feature]- add methods to UITableView
    Remove empty area when has no data
    scrollToLastRow
    ScrollToFirstRow
    ScrollToHeader

--- MemoryChainKit/Sources/Extensions/UIKit/UITableView+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UITableView+Extension.swift
@@ -9,6 +9,27 @@
 import UIKit
 
 public extension UITableView {
+    func scrollToLastRow() {
+        let indexPath = IndexPath(row:0,section: 0)
+        self.scrollToRow(at: indexPath, at: .bottom, animated: true)
+    }
+    func scrollToFirstRow() {
+        let indexPath = IndexPath(row: 0, section: 0)
+        self.scrollToRow(at: indexPath, at: .top, animated: true)
+    }
+    func scrollToSelectedRow() {
+        let selectedRows = self.indexPathsForVisibleRows
+        if let selectedRow = selectedRows?[0] as NSIndexPath? {
+            self.scrollToRow(at: selectedRow as IndexPath, at: .middle, animated: true)
+        }
+    }
+    func scrollToHeader() {
+        self.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: true)
+    }
+    func removeEliminateExtraSeparators() {
+        self.tableFooterView = UIView(frame: .zero)
+        self.tableHeaderView = UIView(frame:.zero)
+    }
     func registerCell(_ cellTypes:[AnyClass]) {
         for cellType in cellTypes {
             let typeString = String(describing: cellType)
