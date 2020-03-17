58253221beaaafaa2c830fa6b52c9d4a58020f27
MarcSteven <marczhao@memoriesus.com>
2020-03-17 16:36:02 +0800

    use struct to check iOS version

---
+++ MemoryChainKit/Sources/Transitions/DeviceVersion.swift
@@ -0,0 +1,20 @@
+//
+//  DeviceVersion.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/3/17.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+import Foundation
+import UIKit
+public struct DeviceVersion {
+    static let system_version_float = (UIDevice.current.systemVersion as NSString).floatValue
+    static let iOS7 = (DeviceVersion.system_version_float < 8.0 && DeviceVersion.system_version_float >= 7.0)
+    static let iOS8 = (DeviceVersion.system_version_float >= 8.0 && DeviceVersion.system_version_float < 9.0)
+    static let iOS9 = (DeviceVersion.system_version_float >= 9.0 && DeviceVersion.system_version_float < 10.0)
+    static let iOS10 = (DeviceVersion.system_version_float >= 10.0 && DeviceVersion.system_version_float <= 11.0)
+    static let iOS11 = (DeviceVersion.system_version_float >= 11.0 && DeviceVersion.system_version_float <= 12.0)
+    static let iOS12 = (DeviceVersion.system_version_float >= 12.0 && DeviceVersion.system_version_float <= 13.0)
+    static let iOS13 = (DeviceVersion.system_version_float >= 13.0 && DeviceVersion.system_version_float <= 14.0)
+}
