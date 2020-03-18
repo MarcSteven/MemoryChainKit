f2a921827c8d2d088a6eb73f4b48e65d6b7e661a
zhaomeiru66 <472207180@qq.com>
2020-03-18 16:43:10 +0800

    Refactor the code to make file less
    Because there are two files about CALayer extension, now merge it into one file

--- MemoryChainKit/Sources/Extensions/UIKit/CALayerExtension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/CALayerExtension.swift
@@ -9,6 +9,23 @@
 import UIKit
 
 public extension CALayer {
+    func addBorder(edge:UIRectEdge,color:UIColor,thickness:CGFloat) {
+        let border = CALayer()
+        switch edge {
+        case UIRectEdge.top:
+            border.frame = CGRect(x: 0, y: 0, width: frame.width, height: thickness)
+        case UIRectEdge.left:
+            border.frame = CGRect(x: 0, y: 0, width: thickness, height: frame.height)
+        case UIRectEdge.bottom:
+            border.frame = CGRect(x: 0, y: frame.height - thickness, width: frame.width, height: thickness)
+        case UIRectEdge.right:
+            border.frame = CGRect(x: frame.width - thickness, y: 0, width: thickness, height: frame.height)
+        default:
+            break
+        }
+        border.backgroundColor = color.cgColor
+        self.addSublayer(border)
+    }
     func applyAnimation(animation:CABasicAnimation) {
         let copy = animation.copy() as! CABasicAnimation
         if copy.fromValue == nil {
