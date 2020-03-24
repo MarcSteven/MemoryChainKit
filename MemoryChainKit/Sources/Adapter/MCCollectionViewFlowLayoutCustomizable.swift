6be33eb8c0009e6951f2ebde8f7038db0ed68e30
MarcSteven <marczhao@memoriesus.com>
2020-03-24 22:31:13 +0800

    add CollectiViewFlowLayout protocol to custom it

---
+++ MemoryChainKit/Sources/Adapter/MCCollectionViewFlowLayoutCustomizable.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/3/24.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+import UIKit
+
+// MARK: - MCCollectionViewFlowLayoutCustomizable
+
+public protocol MCCollectionViewFlowLayoutCustomizable {
+    func sectionInset(_ layout: UICollectionViewLayout, for section: Int) -> UIEdgeInsets
+    func minimumLineSpacing(_ layout: UICollectionViewLayout, for section: Int) -> CGFloat
+    func minimumInteritemSpacing(_ layout: UICollectionViewLayout, for section: Int) -> CGFloat
+}
+
+extension MCCollectionViewFlowLayoutCustomizable {
+    public func sectionInset(_ layout: UICollectionViewLayout, for section: Int) -> UIEdgeInsets {
+        (layout as? UICollectionViewFlowLayout)?.sectionInset ?? .zero
+    }
+
+    public func minimumLineSpacing(_ layout: UICollectionViewLayout, for section: Int) -> CGFloat {
+        (layout as? UICollectionViewFlowLayout)?.minimumLineSpacing ?? 0
+    }
+
+    public func minimumInteritemSpacing(_ layout: UICollectionViewLayout, for section: Int) -> CGFloat {
+        (layout as? UICollectionViewFlowLayout)?.minimumInteritemSpacing ?? 0
+    }
+}
+
