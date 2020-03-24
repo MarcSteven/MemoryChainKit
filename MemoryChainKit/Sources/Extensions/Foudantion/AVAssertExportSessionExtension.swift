60565b97c3ad4c191693545fdf59a5657c337769
MarcSteven <marczhao@memoriesus.com>
2020-03-24 21:52:40 +0800

    add AVAssertExportSession extension to compress video file

---
+++ MemoryChainKit/Sources/Extensions/Foudantion/AVAssertExportSessionExtension.swift
@@ -0,0 +1,27 @@
+//
+//  AVAssertExportSessionExtension.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/3/24.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+import AVFoundation
+
+public extension AVAssetExportSession {
+    func compressVideo(_ inputURL:URL,
+                       outputURL:URL,
+                       completionHandler:@escaping (_ exportSession:AVAssetExportSession?)->Void) {
+        let urlAsset = AVURLAsset(url: inputURL, options: nil)
+        guard let exportSession = AVAssetExportSession(asset: urlAsset, presetName: AVAssetExportPresetLowQuality) else {
+            completionHandler(nil)
+            return
+        }
+        exportSession.outputURL = outputURL
+        exportSession.outputFileType = AVFileType.mov
+        exportSession.shouldOptimizeForNetworkUse = true
+        exportSession.exportAsynchronously {
+            completionHandler(exportSession)
+        }
+    }
+}
