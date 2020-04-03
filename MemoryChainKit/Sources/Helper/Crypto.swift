cea8f34b82af8b6c4adac6d9f31137306a9ea0dd
MarcSteven <marczhao@memoriesus.com>
2020-04-03 23:44:28 +0800

    add  algorithm to make data safer

---
+++ MemoryChainKit/Sources/Helper/Crypto.swift
@@ -0,0 +1,71 @@
+//
+//  Crypto.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/4/3.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+
+import Foundation
+import CommonCrypto
+#if canImport(CryptoKit)
+import CryptoKit
+#endif
+
+@available(iOS 13.0, *)
+extension Digest {
+    public var data: Data {
+        Data(makeIterator())
+    }
+
+    public func hexEncodedString() -> String {
+        makeIterator().map { String(format: "%02hhx", $0) }.joined()
+    }
+}
+
+extension Data {
+    public func md5() -> String {
+        if #available(iOS 13.0, *) {
+            return Insecure.MD5.hash(data: self).hexEncodedString()
+        } else {
+            let hash = withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
+                var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+                CC_MD5(bytes.baseAddress, CC_LONG(count), &hash)
+                return hash
+            }
+
+            return hash.hexEncodedString()
+        }
+    }
+
+    public func sha256() -> String {
+        if #available(iOS 13.0, *) {
+            return SHA256.hash(data: self).hexEncodedString()
+        } else {
+            let hash = withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
+                var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+                CC_SHA256(bytes.baseAddress, CC_LONG(count), &hash)
+                return hash
+            }
+
+            return hash.hexEncodedString()
+        }
+    }
+}
+
+extension String {
+    public func md5() -> String? {
+        data(using: .utf8)?.md5()
+    }
+
+    public func sha256() -> String? {
+        data(using: .utf8)?.sha256()
+    }
+}
+
+extension Array where Element == UInt8 {
+    func hexEncodedString() -> String {
+        map { String(format: "%02hhx", $0) }.joined()
+    }
+}
