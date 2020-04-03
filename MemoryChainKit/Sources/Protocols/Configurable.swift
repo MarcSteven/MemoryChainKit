252f0dcaf09d30ad7c35e46da3e59f5804065a8f
MarcSteven <marczhao@memoriesus.com>
2020-04-03 23:19:32 +0800

    Add protocols

---
+++ MemoryChainKit/Sources/Protocols/Configurable.swift
@@ -0,0 +1,29 @@
+//
+//  Configurable.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/4/3.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+public protocol Configurable {
+    associatedtype ObjectType
+    func configure(_ object:ObjectType)
+}
+
+public struct AnyConfigurable<T> {
+    public let base:Any
+    private let _configure:(_ object:T) ->Void
+    public init<C:Configurable>(_ base:C) where C.ObjectType == T {
+        self.base = base
+        self._configure = {
+            base.configure($0)
+        }
+    }
+}
+
+extension AnyConfigurable:Configurable {
+    public func configure(_ object: T) {
+        _configure(object)
+    }
+}
---
+++ MemoryChainKit/Sources/Protocols/InitializableByEmptyConstructor.swift
@@ -0,0 +1,12 @@
+//
+//  InitializableByEmptyConstructor.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/4/3.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+
+public protocol InitializableByEmptyConstructor:class {
+    init()
+}
