8b6eb7e36b8a4e78a158865456ac8cf27233aed4
MarcSteven <marczhao@memoriesus.com>
2020-04-09 11:58:36 +0800

    [Feature]-String+URL extension

--- MemoryChainKit/Sources/Extensions/Foudantion/StringExtension.swift
+++ MemoryChainKit/Sources/Extensions/Foudantion/StringExtension.swift
@@ -7,7 +7,146 @@
 //
 
 import Foundation
+
 import UIKit
+
+
+
+extension StringProtocol {
+    public func index(from: Int) -> Index? {
+        guard
+            from > -1,
+            let index = self.index(startIndex, offsetBy: from, limitedBy: endIndex)
+        else {
+            return nil
+        }
+
+        return index
+    }
+
+    /// Returns the element at the specified `index` iff it is within bounds,
+    /// otherwise `nil`.
+    public func at(_ index: Int) -> String? {
+        guard let index = self.index(from: index), let character = at(index) else {
+            return nil
+        }
+
+        return String(character)
+    }
+}
+
+// MARK: - `at(:)`
+
+extension String {
+    /// Returns the `Substring` at the specified range iff it is within bounds, otherwise `nil`.
+    ///
+    /// e.g., `"Hello world"[..<5] // → "Hello"`
+    public func at(_ range: PartialRangeUpTo<Int>) -> Substring? {
+        hasIndex(range) ? self[range] : nil
+    }
+
+    /// Returns the `Substring` at the specified range iff it is within bounds, otherwise `nil`.
+    ///
+    /// e.g., `"Hello world"[...4] // → "Hello"`
+    public func at(_ range: PartialRangeThrough<Int>) -> Substring? {
+        hasIndex(range) ? self[range] : nil
+    }
+
+    /// Returns the `Substring` at the specified range iff it is within bounds, otherwise `nil`.
+    ///
+    /// e.g., `"Hello world"[0...] // → "Hello world"`
+    public func at(_ range: PartialRangeFrom<Int>) -> Substring? {
+        hasIndex(range) ? self[range] : nil
+    }
+
+    /// Returns the `Substring` at the specified range iff it is within bounds, otherwise `nil`.
+    ///
+    /// e.g., `"Hello world"[0..<5] // → "Hello"`
+    public func at(_ range: CountableRange<Int>) -> Substring? {
+        hasIndex(range) ? self[range] : nil
+    }
+
+    /// Returns the `Substring` at the specified range iff it is within bounds, otherwise `nil`.
+    ///
+    /// e.g., `"Hello world"[0...4] // → "Hello"`
+    public func at(range: CountableClosedRange<Int>) -> Substring? {
+        hasIndex(range) ? self[range] : nil
+    }
+}
+
+// MARK: - Ranges
+
+extension String {
+    /// e.g., `"Hello world"[..<5] // → "Hello"`
+    private subscript(range: PartialRangeUpTo<Int>) -> Substring {
+        self[..<index(startIndex, offsetBy: range.upperBound)]
+    }
+
+    /// e.g., `"Hello world"[...4] // → "Hello"`
+    private subscript(range: PartialRangeThrough<Int>) -> Substring {
+        self[...index(startIndex, offsetBy: range.upperBound)]
+    }
+
+    /// e.g., `"Hello world"[0...] // → "Hello world"`
+    private subscript(range: PartialRangeFrom<Int>) -> Substring {
+        self[index(startIndex, offsetBy: range.lowerBound)...]
+    }
+
+    /// e.g., `"Hello world"[0..<5] // → "Hello"`
+    private subscript(range: CountableRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(startIndex, offsetBy: range.upperBound)
+        return self[start..<end]
+    }
+
+    /// e.g., `"Hello world"[0...4] // → "Hello"`
+    private subscript(range: CountableClosedRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(startIndex, offsetBy: range.upperBound)
+        return self[start...end]
+    }
+}
+
+
+
+// MARK: - `hasIndex(:)`
+
+extension String {
+    /// Return true iff range is in `self`.
+    private func hasIndex(_ range: PartialRangeUpTo<Int>) -> Bool {
+        range.upperBound >= firstIndex && range.upperBound < lastIndex
+    }
+
+    /// Return true iff range is in `self`.
+    private func hasIndex(_ range: PartialRangeThrough<Int>) -> Bool {
+        range.upperBound >= firstIndex && range.upperBound < lastIndex
+    }
+
+    /// Return true iff range is in `self`.
+    private func hasIndex(_ range: PartialRangeFrom<Int>) -> Bool {
+        range.lowerBound >= firstIndex && range.lowerBound < lastIndex
+    }
+
+    /// Return true iff range is in `self`.
+    private func hasIndex(_ range: CountableRange<Int>) -> Bool {
+        range.lowerBound >= firstIndex && range.upperBound < lastIndex
+    }
+
+    /// Return true iff range is in `self`.
+    private func hasIndex(_ range: CountableClosedRange<Int>) -> Bool {
+        range.lowerBound >= firstIndex && range.upperBound < lastIndex
+    }
+}
+
+extension String {
+    private var firstIndex: Int {
+        startIndex.utf16Offset(in: self)
+    }
+
+    private var lastIndex: Int {
+        endIndex.utf16Offset(in: self)
+    }
+}
 
 extension String {
     private var nsString:NSString {
---
+++ MemoryChainKit/Sources/Extensions/Foudantion/URLExtension.swift
@@ -0,0 +1,66 @@
+//
+//  URLExtension.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/4/9.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    public init?(string: String, queryParameters: [String: String]) {
+        var components = URLComponents(string: string)
+        components?.queryItems = queryParameters.map(URLQueryItem.init)
+
+        guard let string = components?.url?.absoluteString else {
+            return nil
+        }
+
+        self.init(string: string)
+    }
+}
+
+
+// MARK: - Scheme
+
+extension URL {
+    /// The scheme of the `URL`.
+    public var schemeType: Scheme {
+        guard let scheme = scheme else {
+            return .none
+        }
+
+        return Scheme(rawValue: scheme)
+    }
+}
+
+extension URL {
+    public struct Scheme: RawRepresentable, Hashable, CustomStringConvertible {
+        public let rawValue: String
+
+        public init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+
+        public var description: String {
+            rawValue
+        }
+    }
+}
+
+extension URL.Scheme: ExpressibleByStringLiteral {
+    public init(stringLiteral value: StringLiteralType) {
+        self.rawValue = value
+    }
+}
+
+extension URL.Scheme {
+    public static let none: Self = ""
+    public static let https: Self = "https"
+    public static let http: Self = "http"
+    public static let file: Self = "file"
+    public static let tel: Self = "tel"
+    public static let sms: Self = "sms"
+    public static let email: Self = "mailto"
+}
