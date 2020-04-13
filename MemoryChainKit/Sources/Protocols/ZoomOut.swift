aefc3f2e7174a65bc0563f67d03a3e0795a1abd3
MarcSteven <marczhao@memoriesus.com>
2020-04-13 13:48:20 +0800

    Refactor UIView Zoom Animation

---
+++ MemoryChainKit/Sources/Protocols/ZoomOut.swift
@@ -0,0 +1,49 @@
+//
+//  ZoomOut.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/4/13.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+import UIKit
+
+
+
+public protocol ZoomOut {}
+
+
+extension ZoomOut where Self:UIView {
+    /**
+     Simply zooming out of a view: set view scale to Identity and zoom out to 0 on 'duration' time interval.
+     - parameter duration: animation duration
+     */
+     func zoomOut(duration: TimeInterval = 0.2) {
+     self.transform = CGAffineTransform.identity
+     UIView.animate(withDuration: duration, delay: 0.0, options: [.curveLinear], animations: { () -> Void in
+         self.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
+            }) { (animationCompleted: Bool) -> Void in
+        }
+    }
+
+    
+    /**
+     Zoom out any view with specified offset magnification.
+     - parameter duration:     animation duration.
+     - parameter easingOffset: easing offset.
+     */
+    func zoomOutWithEasing( duration: TimeInterval = 0.2, easingOffset: CGFloat = 0.2) {
+        let easeScale = 1.0 + easingOffset
+        let easingDuration = TimeInterval(easingOffset) * duration / TimeInterval(easeScale)
+        let scalingDuration = duration - easingDuration
+     UIView.animate(withDuration: easingDuration, delay: 0.0, options: .curveEaseOut, animations: { () -> Void in
+         self.transform = CGAffineTransform(scaleX: easeScale, y: easeScale)
+            }, completion: { (completed: Bool) -> Void in
+             UIView.animate(withDuration: scalingDuration, delay: 0.0, options: .curveEaseOut, animations: { () -> Void in
+                 self.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
+                    }, completion: { (completed: Bool) -> Void in
+                })
+        })
+    }
+
+}
