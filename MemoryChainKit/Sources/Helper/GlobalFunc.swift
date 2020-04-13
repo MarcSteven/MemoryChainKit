f56fe11c0ae839335844ee69068de5668a8d90bd
MarcSteven <marczhao@memoriesus.com>
2020-04-13 11:12:06 +0800

    Refactor the code to make it global function

--- MemoryChainKit/Sources/Helper/GlobalFunc.swift
+++ MemoryChainKit/Sources/Helper/GlobalFunc.swift
@@ -14,3 +14,20 @@ public func unsafePointer<T:AnyObject>(to object:T) ->UnsafeRawPointer {
 public func unsafeMutablePointer<T:AnyObject>(to object:T)->UnsafeMutableRawPointer {
     return Unmanaged<T>.passUnretained(object).toOpaque()
 }
+/// Detects if the LLDB debugger is attached to the app.
+///
+/// - Note: LLDB is automatically attached when the app is running from Xcode.
+public var isDebuggerAttached: Bool {
+    var info = kinfo_proc()
+    var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+    var size = MemoryLayout<kinfo_proc>.stride
+    let junk = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
+    assert(junk == 0, "sysctl failed")
+    return (info.kp_proc.p_flag & P_TRACED) != 0
+}
+
+public func synchronized<T>(_ lock: AnyObject, _ closure: () throws -> T) rethrows -> T {
+    objc_sync_enter(lock)
+    defer { objc_sync_exit(lock) }
+    return try closure()
+}
