202bf1a7320918ed8b5ff9d55fd172625e123c2b
MarcSteven <marczhao@memoriesus.com>
2020-04-13 16:40:05 +0800

    add userDefaults propertyWrapper

---
+++ MemoryChainKit/Sources/Helper/UserDefaults.swift
@@ -0,0 +1,33 @@
+//
+//  UserDefaults.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/4/13.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+import Foundation
+
+
+//Usage:
+
+/// enum GlobalSettings {
+//@UserDefaults(key:"is_first_launch",defaultValue:false)
+//static var isFirstLaunch:Bool
+//}
+
+
+@propertyWrapper
+
+public struct UserDefaults<Value> {
+    public let key:String
+    public let defaultValue:Value
+    public var wrappedValue:Value {
+        get {
+            UserDefaults.standard.object(forKey:key) as? Value ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
