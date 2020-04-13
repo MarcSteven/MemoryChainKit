021e0465dc2cee397e28f87f9fff2029db51cf24
MarcSteven <marczhao@memoriesus.com>
2020-04-13 13:39:17 +0800

    Refactor the code about UILabel animate method
    Use protocol to do instead of extension

--- MemoryChainKit/Sources/Protocols/Animatable.swift
+++ MemoryChainKit/Sources/Protocols/Animatable.swift
@@ -82,3 +82,26 @@ public extension Animatable where Self: UIView {
     }
     
 }
+extension Animatable where Self:UILabel {
+    func animate(font:UIFont,duration:TimeInterval) {
+        let oldFrame = frame
+        let labelScale = self.font.pointSize / font.pointSize
+        self.font = font
+        let oldTransform = transform
+        transform = transform.scaledBy(x: labelScale, y: labelScale)
+        let newOrigin = frame.origin
+        if self.textAlignment == .left || self.textAlignment == .natural {
+            frame.origin = oldFrame.origin
+        }
+        if self.textAlignment == .right {
+            frame.origin = CGPoint(x: oldFrame.origin.x + oldFrame.width - frame.width, y: oldFrame.origin.y)
+        }
+        setNeedsUpdateConstraints()
+        UIView.animate(withDuration: duration) {
+            self.frame.origin = newOrigin
+            self.transform = oldTransform
+            self.layoutIfNeeded()
+        }
+    }
+
+}
