708727456782b02e16ad536d41ed1854a92940db
Marc Steven <MarcSteven@users.noreply.github.com>
2020-04-19 23:18:42 +0800

    add method to extract substring from a String
    extract substrings from a string that matches a regex pattern.

--- MemoryChainKit/Sources/Extensions/Foudantion/StringExtension.swift
+++ MemoryChainKit/Sources/Extensions/Foudantion/StringExtension.swift
@@ -7,10 +7,36 @@
 //
 
 import Foundation
-
 import UIKit
 
 
+extension String {
+   /// extract substrings from a string that match a regex pattern.
+   
+       func regex (pattern: String) -> [String] {
+         do {
+            let regex = try NSRegularExpression(pattern: pattern, options: NSRegularExpression.Options(rawValue: 0))
+           let nsstr = self as NSString
+           let all = NSRange(location: 0, length: nsstr.length)
+           var matches : [String] = [String]()
+            regex.enumerateMatches(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: all) {
+             (result : NSTextCheckingResult?, _, _) in
+             if let r = result {
+                let result = nsstr.substring(with: r.range) as String
+               matches.append(result)
+             }
+           }
+           return matches
+         } catch {
+           return [String]()
+         }
+       }
+
+      public func isMatch(_ pattern: String) -> Bool {
+        return  !regex(pattern: pattern).isEmpty
+     
+}
+}
 
 extension StringProtocol {
     public func index(from: Int) -> Index? {
