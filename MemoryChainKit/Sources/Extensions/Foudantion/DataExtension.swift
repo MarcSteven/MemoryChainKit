be0cf2f6c6ad31dba92f9149b7e466e5030fed35
MarcSteven <marcsteven@memoriesus.com>
2020-04-21 19:31:49 +0800

    add convenience method to Data
    A convenience method to append string to `Data` using specified encoding.
    
    Signed-off-by: MarcSteven <marcsteven@memoriesus.com>

--- MemoryChainKit/Sources/Extensions/Foudantion/DataExtension.swift
+++ MemoryChainKit/Sources/Extensions/Foudantion/DataExtension.swift
@@ -30,3 +30,18 @@ public extension Data {
         return map {String(format: "%02hhx",$0)}.joined()
     }
 }
+
+extension Data {
+    /// A convenience method to append string to `Data` using specified encoding.
+    ///
+    /// - Parameters:
+    ///   - string: The string to be added to the `Data`.
+    ///   - encoding: The encoding to use for representing the specified string.
+    ///               The default value is `.utf8`.
+    ///   - allowLossyConversion: A boolean value to determine lossy conversion.
+    ///                           The default value is `false`.
+    public mutating func append(_ string: String, encoding: String.Encoding = .utf8, allowLossyConversion: Bool = false) {
+        guard let newData = string.data(using: encoding, allowLossyConversion: allowLossyConversion) else { return }
+        append(newData)
+    }
+}
