f5e8cca472cbae2cb59a90bcc23b031b548a6dd9
MarcSteven <marcsteven@memoriesus.com>
2020-04-22 10:58:07 +0800

    add protocol to getAssociated Property

---
+++ MemoryChainKit/Sources/Protocols/PropertyStoring.swift
@@ -0,0 +1,25 @@
+//
+//  PropertyStoring.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/4/22.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+import Foundation
+
+public protocol PropertyStoring {
+    associatedtype T
+    func getAssociatedObject(_ key:UnsafeRawPointer!,
+                             defaultValue:T) ->T
+}
+
+extension PropertyStoring {
+    func getAssociatedObject(_ key:UnsafeRawPointer!,
+                             defaultValue:T) ->T {
+        guard  let value = objc_getAssociatedObject(self, key) as? T else {
+            return defaultValue
+        }
+        return value
+    }
+}
