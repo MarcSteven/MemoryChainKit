0832d2afccaef04e676e8da1a721090ec9fe2a44
MarcSteven <marcsteven@memoriesus.com>
2020-04-26 21:41:06 +0800

    add box

---
+++ MemoryChainKit/Sources/Store/Cache/Box.swift
@@ -0,0 +1,16 @@
+//
+//  Box.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2020/4/24.
+//  Copyright © 2020 Marc Steven(https://github.com/MarcSteven). All rights reserved.
+//
+
+import Foundation
+
+public final class Box<T> {
+    let value:T
+    init(_  value:T) {
+        self.value = value
+    }
+}
