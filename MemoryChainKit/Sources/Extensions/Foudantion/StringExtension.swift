ed1c8666a41f28c71bad0cb3ec6104bb994be271
Marc Steven <MarcSteven@users.noreply.github.com>
2020-05-08 08:52:03 +0800

    add validate extension method

--- MemoryChainKit/Sources/Extensions/Foudantion/StringExtension.swift
+++ MemoryChainKit/Sources/Extensions/Foudantion/StringExtension.swift
@@ -495,3 +495,22 @@ extension String {
            }
 
 }
+
+// MARK: - Convenience Extension
+
+extension String {
+    /// Returns a boolean value indicating whether the `self` matches the conditions
+    /// specified by the `rule`.
+    ///
+    /// ```swift
+    /// "help@example.com".validate(rule: .email) // valid
+    /// "help.example.com".validate(rule: .email) // invalid
+    /// ```
+    ///
+    /// - Parameter rule: The rule against which to evaluate `self`.
+    /// - Returns: `true` if `self` matches the conditions specified by the given
+    ///            `rule`, otherwise `false`.
+    public func validate(rule: ValidationRule<String>) -> Bool {
+        rule.validate(self)
+    }
+}
