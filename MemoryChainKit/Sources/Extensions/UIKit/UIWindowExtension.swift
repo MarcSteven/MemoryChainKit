ab7705058039e45b7af092e47adc006f7cf9724f
Marc Steven <MarcSteven@users.noreply.github.com>
2020-09-19 21:26:47 +0800

    add class properties for UIWindow

--- MemoryChainKit/Sources/Extensions/UIKit/UIWindowExtension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIWindowExtension.swift
@@ -25,3 +25,15 @@ public extension UIWindow {
         return top
     }
 }
+public extension UIWindow {
+  
+  class var key: UIWindow {
+    guard let keyWindow = UIApplication.shared.keyWindow else { fatalError("Fatal Error: now window is set to keyWindow") }
+    return keyWindow
+  }
+  
+  class var keySafeAreaInsets: UIEdgeInsets {
+    guard #available(iOS 11.0, *) else { return .zero }
+    return UIWindow.key.safeAreaInsets
+  }
+}
