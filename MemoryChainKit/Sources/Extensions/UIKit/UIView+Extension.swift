21a451cf5e213d27a060af030ffb9e7722ecbf87
Marc Steven <MarcSteven@users.noreply.github.com>
2020-09-20 09:44:02 +0800

    add convenience method to add constraints

--- MemoryChainKit/Sources/Extensions/UIKit/UIView+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIView+Extension.swift
@@ -342,3 +342,38 @@ private func debugSubviews(_ count: Int = 0) {
     }
 }
 }
+
+public extension UIView {
+  
+  func pinEdgesToSuperview() {
+    translatesAutoresizingMaskIntoConstraints = false
+    guard let superview = self.superview else { return }
+    let constraints = NSLayoutConstraint.pinningEdgesToSuperview(view: self)
+    superview.addConstraints(constraints)
+  }
+  
+  func pinToSuperview(forAtrributes attributes: [NSLayoutConstraint.Attribute], multiplier: CGFloat = 1.0, constant: CGFloat = 0.0) {
+    translatesAutoresizingMaskIntoConstraints = false
+    guard let superview = self.superview else { return }
+    let constraints = NSLayoutConstraint.pinningToSuperview(view: self, attributes: attributes, multiplier: multiplier, constant: constant)
+    superview.addConstraints(constraints)
+  }
+  
+  func pin(toView: UIView, attributes: [NSLayoutConstraint.Attribute], multiplier: CGFloat = 1.0, constant: CGFloat = 0.0) {
+    translatesAutoresizingMaskIntoConstraints = false
+    guard let superview = self.superview else { return }
+    let constraints = NSLayoutConstraint.pinning(view: self, toView: toView, attributes: attributes, multiplier: multiplier, constant: constant)
+    superview.addConstraints(constraints)
+  }
+  
+  func pin(attribute: NSLayoutConstraint.Attribute, toView: UIView?, toAttribute: NSLayoutConstraint.Attribute, multiplier: CGFloat = 1.0, constant: CGFloat = 0.0) {
+    translatesAutoresizingMaskIntoConstraints = false
+    guard let superview = self.superview else { return }
+    let constraint = NSLayoutConstraint.pinning(view: self, attribute: attribute, toView: toView, toAttribute: toAttribute, multiplier: multiplier, constant: constant)
+    superview.addConstraint(constraint)
+  }
+}
+
+
+
+
