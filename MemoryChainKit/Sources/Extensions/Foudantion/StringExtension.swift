4893b4e6c175ac3851879736591591b52b35df02
Marc Steven <MarcSteven@users.noreply.github.com>
2020-09-27 22:57:10 +0800

    add method to check url has image

--- MemoryChainKit/Sources/Extensions/Foudantion/StringExtension.swift
+++ MemoryChainKit/Sources/Extensions/Foudantion/StringExtension.swift
@@ -531,6 +531,35 @@ public extension String {
       return nil
     }
   }
+public  extension String {
+
+     func isImage() -> Bool {
+        // Add here your image formats.
+        let imageFormats = ["jpg", "jpeg", "png", "gif"]
+
+        if let ext = self.getExtension() {
+            return imageFormats.contains(ext)           
+        }
+
+        return false
+    }
+
+     func getExtension() -> String? {
+       let ext = (self as NSString).pathExtension
+
+       if ext.isEmpty {
+           return nil
+       }
+
+       return ext
+    }
+
+     func isURL() -> Bool {
+       return URL(string: self) != nil
+    }
+
+}
+
 }
 
 
