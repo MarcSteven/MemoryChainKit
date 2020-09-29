0b1192a667fe9e978332399164383241a9af74fe
Marc Steven <MarcSteven@users.noreply.github.com>
2020-09-29 15:46:49 +0800

    add format mobile method

--- MemoryChainKit/Sources/Helper/GlobalFunc.swift
+++ MemoryChainKit/Sources/Helper/GlobalFunc.swift
@@ -230,3 +230,23 @@ public func dictionaryOfNames(_ array:UIView...) ->[String:UIView] {
     return dictionary
 }
 
+func format(with mask: String, phone: String) -> String {
+    let numbers = phone.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
+    var result = ""
+    var index = numbers.startIndex // numbers iterator
+
+    // iterate over the mask characters until the iterator of numbers ends
+    for ch in mask where index < numbers.endIndex {
+        if ch == "X" {
+            // mask requires a number in this place, so take the next one
+            result.append(numbers[index])
+
+            // move numbers iterator to the next index
+            index = numbers.index(after: index)
+
+        } else {
+            result.append(ch) // just append a mask character
+        }
+    }
+    return result
+}
