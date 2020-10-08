c5ddc1a0d17e263d79d6ac2c8f6b209e2fd767ce
Marc Steven <MarcSteven@users.noreply.github.com>
2020-10-08 13:29:19 +0800

    add property(current responder) for UIResponder

--- MemoryChainKit/Sources/Extensions/UIKit/UIResponderExtension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIResponderExtension.swift
@@ -43,3 +43,19 @@ extension UIResponder {
         return nil
     }
 }
+@objc
+public extension UIResponder {
+   
+        private static weak var _currentFirstResponder: UIResponder?
+        static var currentFirstResponder: UIResponder? {
+            _currentFirstResponder = nil
+            // Passing `nil` to the to paramter of `sendAction` calls it on the firstResponder.
+            UIApplication.shared.sendAction(#selector(findFirstResponder), to: nil, from: nil, for: nil)
+            return _currentFirstResponder
+        }
+
+        private func findFirstResponder() {
+            UIResponder._currentFirstResponder = self
+        }
+    }
+
