2158550adc0c335b4f57273cf4bd28c507d0f1c4
Marc Steven <MarcSteven@users.noreply.github.com>
2020-10-09 22:27:04 +0800

    add  method to fill superview and center

--- MemoryChainKit/Sources/Extensions/UIKit/UIView+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIView+Extension.swift
@@ -452,5 +452,42 @@ public extension UIView {
     }
 }
 
-
+public extension UIView {
+  func fillSuperview(_ superview: UIView, withConstant constant: CGFloat = 0) {
+    translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate(
+      [leadingAnchor.constraint(
+        equalTo: superview.leadingAnchor,
+        constant: constant),
+       topAnchor.constraint(
+        equalTo: superview.topAnchor,
+        constant: constant),
+       trailingAnchor.constraint(
+        equalTo: superview.trailingAnchor,
+        constant: -constant),
+       bottomAnchor.constraint(
+        equalTo: superview.bottomAnchor,
+        constant: -constant)]
+    )
+  }
+  
+  func center(_ superview: UIView) {
+    translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate(
+      [centerXAnchor.constraint(
+        equalTo: superview.centerXAnchor),
+       centerYAnchor.constraint(
+        equalTo: superview.centerYAnchor),
+       topAnchor.constraint(
+        greaterThanOrEqualTo: superview.topAnchor),
+       leadingAnchor.constraint(
+        greaterThanOrEqualTo: superview.leadingAnchor),
+       trailingAnchor.constraint(
+        lessThanOrEqualTo: superview.trailingAnchor),
+       bottomAnchor.constraint(
+        lessThanOrEqualTo: superview.bottomAnchor)
+      ]
+    )
+  }
+}
 
