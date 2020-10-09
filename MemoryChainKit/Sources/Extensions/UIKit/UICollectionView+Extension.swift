68f7f44fd5be0a1ca98477af6c933ccaf57972bf
Marc Steven <MarcSteven@users.noreply.github.com>
2020-10-09 12:14:32 +0800

    add method to get visible cell in CollectionView

--- MemoryChainKit/Sources/Extensions/UIKit/UICollectionView+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UICollectionView+Extension.swift
@@ -106,4 +106,18 @@ extension UICollectionView {
         cell { $0.isKind(of: kind) } as? T
     }
 }
+public extension UICollectionView {
 
+    /// VisibleCells in the order they are displayed on screen.
+    var orderedVisibleCells: [UICollectionViewCell] {
+        return indexPathsForVisibleItems.sorted().compactMap { cellForItem(at: $0) }
+    }
+
+    /// Gets the currently visibleCells of a section.
+    ///
+    /// - Parameter section: The section to filter the cells.
+    /// - Returns: Array of visible UICollectionViewCells in the argument section.
+    func visibleCells(in section: Int) -> [UICollectionViewCell] {
+        return visibleCells.filter { indexPath(for: $0)?.section == section }
+    }
+}
