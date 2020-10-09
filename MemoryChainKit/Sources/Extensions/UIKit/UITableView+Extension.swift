3a70264922c194974a0faf4ab707c0a603b485ba
Marc Steven <MarcSteven@users.noreply.github.com>
2020-10-09 12:08:59 +0800

    add method to get the visible cell

--- MemoryChainKit/Sources/Extensions/UIKit/UITableView+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UITableView+Extension.swift
@@ -195,3 +195,13 @@ extension UITableView {
         targetContentOffset.pointee.y = offsetY
     }
 }
+public extension UITableView {
+
+    /// Gets the currently visibleCells of a section.
+    ///
+    /// - Parameter section: The section to filter the cells.
+    /// - Returns: Array of visible UITableViewCell in the argument section.
+    func visibleCells(in section: Int) -> [UITableViewCell] {
+        return visibleCells.filter { indexPath(for: $0)?.section == section }
+    }
+}
