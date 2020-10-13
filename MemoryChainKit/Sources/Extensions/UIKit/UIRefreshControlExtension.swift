55570aee082575f4fecd241214f1d55b868b4eb2
MarcSteven <marcsteven@memoriesus.com>
2020-10-13 12:26:37 +0800

    add method to pasue refresh for UIRefreshControl

--- MemoryChainKit/Sources/Extensions/UIKit/UIRefreshControlExtension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIRefreshControlExtension.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension UIRefreshControl {
+public extension UIRefreshControl {
     private struct AssociatedKey {
         static var timeOutTimer = "timeOutTimer"
     }
@@ -21,5 +21,23 @@ extension UIRefreshControl {
         }
     }
     
-    
+    /// Tells the control that a refresh operation has ended.
+    ///
+    /// Call this method at the end of any refresh operation (whether it was initiated
+    /// programmatically or by the user) to return the refresh control to its default state.
+    /// If the refresh control is at least partially visible, calling this method also hides
+    /// it. If animations are also enabled, the control is hidden using an animation.
+    ///
+    /// - Parameters:
+    ///   - timeoutInterval: The delay before end refreshing.
+    ///   - completion: A closure to execute after end refreshing.
+     func endRefreshing(after timeoutInterval: TimeInterval, completion: (() -> Void)? = nil) {
+        timeoutTimer?.invalidate()
+        timeoutTimer = Timer.schedule(delay: timeoutInterval) { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.endRefreshing()
+            completion?()
+        }
+    }
+
 }
