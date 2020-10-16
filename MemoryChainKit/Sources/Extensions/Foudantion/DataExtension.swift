bb260834ccd92829599356caf674b24bc942e985
Marc Steven <MarcSteven@users.noreply.github.com>
2020-10-16 09:25:47 +0800

    add properties For Data

--- MemoryChainKit/Sources/Extensions/Foudantion/DataExtension.swift
+++ MemoryChainKit/Sources/Extensions/Foudantion/DataExtension.swift
@@ -45,3 +45,36 @@ extension Data {
         append(newData)
     }
 }
+
+public extension Data {
+
+    var string: String? {
+        return String(data: self, encoding: .utf8)
+    }
+
+    var jsonString: String? {
+        guard let json = json, JSONSerialization.isValidJSONObject(json) else {
+            print("Invalid JSON")
+            return nil
+        }
+        do {
+            let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+            return data.string
+        } catch {
+            print(error)
+        }
+
+        return nil
+    }
+
+    var json: [String:Any]? {
+        do {
+            let json = try JSONSerialization.jsonObject(with: self, options: []) as? [String:Any]
+            return json
+        } catch {
+            print(error)
+        }
+
+        return nil
+    }
+}
