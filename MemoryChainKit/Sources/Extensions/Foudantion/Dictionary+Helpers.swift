08c24bfc8fbc4f7aa6a95b33fbcc0012a79aee58
Marc Steven <MarcSteven@users.noreply.github.com>
2020-10-16 09:28:07 +0800

    add properties for Dictionary

--- MemoryChainKit/Sources/Extensions/Foudantion/Dictionary+Helpers.swift
+++ MemoryChainKit/Sources/Extensions/Foudantion/Dictionary+Helpers.swift
@@ -72,5 +72,30 @@ public extension Dictionary where Key: ExpressibleByStringLiteral, Value: Any {
     }
 }
 
+public extension Dictionary where Key == String {
 
+    var jsonString: String? {
+        guard let data = data else {
+            return nil
+        }
+        return String(data: data, encoding: String.Encoding.utf8)
+    }
+
+    var data: Data? {
+
+        guard JSONSerialization.isValidJSONObject(self) else {
+            print("Invalid JSON")
+            return nil
+        }
+
+        do {
+            let data = try JSONSerialization.data(withJSONObject: self, options: .prettyPrinted)
+            return data
+        } catch {
+            print(error)
+        }
+
+        return nil
+    }
+}
 
