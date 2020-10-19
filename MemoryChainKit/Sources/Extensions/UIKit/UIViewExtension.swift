2307e6b431ddd49dc120c281b775d891846bcc56
Marc Steven <MarcSteven@users.noreply.github.com>
2020-10-19 10:10:17 +0800

    add methods for UIView extension

--- MemoryChainKit/Sources/Extensions/UIKit/UIViewExtension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIViewExtension.swift
@@ -226,6 +226,35 @@ public extension UIView {
     func orientationHeight() ->CGFloat {
         return UIApplication.shared.statusBarOrientation.isLandscape ? width : height
     }
+    /// Load Xib from name
+    static func loadFrom<T: UIView>(nibNamed: String, bundle : Bundle? = nil) -> T? {
+        let nib = UINib(nibName: nibNamed, bundle: bundle)
+        return nib.instantiate(withOwner: nil, options: nil)[0] as? T
+    }
+    func getConstraint(byAttribute attr: NSLayoutConstraint.Attribute) -> NSLayoutConstraint? {
+        return constraints.filter { $0.firstAttribute == attr }.first
+    }
+    /// Clear all constraints
+    func clearConstraints() {
+        constraints.forEach { $0.autoRemove() }
+    }
+    /// clear all the subview 
+    func clearAllSubview() {
+                if let stackView = self as? UIStackView {
+            stackView.arrangedSubviews.forEach { view in
+                (view as? IDestroyable)?.destroy()
+                stackView.removeArrangedSubview(view)
+                view.removeFromSuperview()
+            }
+        } else {
+            subviews.forEach { view in
+                (view as? IDestroyable)?.destroy()
+                view.removeFromSuperview()
+            }
+        }
+
+    }
+    
 }
 
 
