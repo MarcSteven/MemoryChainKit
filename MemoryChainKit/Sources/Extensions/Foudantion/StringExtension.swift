a3ab19d3cca89eb54efff15b2ef5bfd4b3a56ea8
Marc Steven <MarcSteven@users.noreply.github.com>
2020-10-21 12:47:53 +0800

    add String extension methods

--- MemoryChainKit/Sources/Extensions/Foudantion/StringExtension.swift
+++ MemoryChainKit/Sources/Extensions/Foudantion/StringExtension.swift
@@ -529,3 +529,32 @@ public extension String {
         return fileURL.lastPathComponent
     }
 }
+public extension String {
+    
+    func appending(_ params: [String: String]) -> String {
+        return appending(self, params)
+    }
+    
+    func appending(_ url: String, _ params: [String: String]) -> String {
+        guard var components = URLComponents(string: url) else {
+            return url
+        }
+        
+        let query = components.percentEncodedQuery ?? ""
+        let temp = params.compactMap({
+            guard !$0.isEmpty, !$1.isEmpty else { return nil }
+            guard let _ = Foundation.URL(string: $1) else {
+                let encoded = $1.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? $1
+                return "\($0)=\(encoded)"
+            }
+            
+            let string = "?!@#$^&%*+,:;='\"`<>()[]{}/\\| "
+            let character = CharacterSet(charactersIn: string).inverted
+            let encoded = $1.addingPercentEncoding(withAllowedCharacters: character) ?? $1
+            return "\($0)=\(encoded)"
+        }).joined(separator: "&")
+        components.percentEncodedQuery = query.isEmpty ? temp : query + "&" + temp
+        return components.url?.absoluteString ?? url
+    }
+}
+
