2233614502171dc7c59b5a92e9b32acc07baf130
Marc Steven <MarcSteven@users.noreply.github.com>
2020-10-23 19:46:04 +0800

    add method to convert Int to Double

---
+++ MemoryChainKit/Sources/Extensions/Foudantion/IntExtension.swift
@@ -0,0 +1,15 @@
+// Created by Marc Steven on 10/23/2020
+
+
+
+import Foundation 
+
+
+public extension Int {
+  /// convert Int to double 
+  func toDouble() -> Double {
+        Double(self)
+    }
+
+
+}
