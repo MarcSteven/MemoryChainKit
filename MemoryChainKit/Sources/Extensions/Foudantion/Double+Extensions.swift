2890d954c36eed62f3bebaf6877cd6e581bf7bbb
Marc Steven <MarcSteven@users.noreply.github.com>
2020-10-23 23:32:23 +0800

    add method to convert to string and price

--- MemoryChainKit/Sources/Extensions/Foudantion/Double+Extensions.swift
+++ MemoryChainKit/Sources/Extensions/Foudantion/Double+Extensions.swift
@@ -24,4 +24,17 @@ public extension Double {
     var ft:Double {
         return self / 3.28084
     }
+    func toString() -> String {
+        String(format: "%.02f", self)
+    }
+    func toPrice(currency: String) -> String {
+        let nf = NumberFormatter()
+        nf.decimalSeparator = ","
+        nf.groupingSeparator = "."
+        nf.groupingSize = 3
+        nf.usesGroupingSeparator = true
+        nf.minimumFractionDigits = 2
+        nf.maximumFractionDigits = 2
+        return (nf.string(from: NSNumber(value: self)) ?? "?") + currency
+    }
 }
