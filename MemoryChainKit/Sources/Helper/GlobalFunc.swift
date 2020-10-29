51c866cccb558fc64b58c18047192b00ffa52ef9
MarcSteven <marcsteven@memoriesus.com>
2020-10-29 11:34:48 +0800

    add method to generate thumbnail image

--- MemoryChainKit/Sources/Helper/GlobalFunc.swift
+++ MemoryChainKit/Sources/Helper/GlobalFunc.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import AVFoundation
 import SafariServices
+import Photos
 
 /// Attempts to open the resource at the specified URL.
 ///
@@ -346,3 +347,23 @@ public func convertFromCAMediaTimingFunctionName(_ input: CAMediaTimingFunctionN
 public func convertToCAMediaTimingFunctionName(_ input: String) -> CAMediaTimingFunctionName {
     return CAMediaTimingFunctionName(rawValue: input)
 }
+
+
+public func getAssetThumbnail(asset: PHAsset, size: CGFloat) -> UIImage {
+    let retinaScale = UIScreen.main.scale
+        let retinaSquare = CGSize(width: size * retinaScale, height: size * retinaScale)
+    let cropSizeLength = min(asset.pixelWidth, asset.pixelHeight)
+        let square = CGRect(x: 0, y: 0, width: CGFloat(cropSizeLength), height: CGFloat(cropSizeLength))
+        let cropRect = square.applying(CGAffineTransform(scaleX: 1.0/CGFloat(asset.pixelWidth), y: 1.0/CGFloat(asset.pixelHeight)))
+    let manager = PHImageManager.default()
+    let options = PHImageRequestOptions()
+    var thumbnail = UIImage()
+        options.isSynchronous = true
+        options.deliveryMode = .highQualityFormat
+            options.resizeMode = .exact
+            options.normalizedCropRect = cropRect
+        manager.requestImage(for: asset, targetSize: retinaSquare, contentMode: .aspectFit, options: options, resultHandler: {(result, info)->Void in
+                thumbnail = result!
+            })
+    return thumbnail
+    }
