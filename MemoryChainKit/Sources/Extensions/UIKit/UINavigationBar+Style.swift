fd3f87df6f0867a2f571f8ef8e55c1a88d332038
Marc Steven <MarcSteven@users.noreply.github.com>
2020-12-04 21:44:01 +0800

    refactor the UINavigationBar extension

--- MemoryChainKit/Sources/Extensions/UIKit/UINavigationBar+Style.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UINavigationBar+Style.swift
@@ -41,3 +41,28 @@ extension UINavigationBar {
     }
     
 }
+public extension UINavigationBar {
+    func hideBottomHairline() {
+        let navigationBarImageView = hairlineImageViewInNavigationBar(view: self)
+        navigationBarImageView?.isHidden = true
+    }
+    func showBottomHairLine() {
+        
+        let navigationBarImageView = hairlineImageViewInNavigationBar(view: self)
+        navigationBarImageView?.isHidden = false 
+    }
+    func changeBottomHairImage() {}
+    private func hairlineImageViewInNavigationBar(view: UIView) -> UIImageView? {
+        if let view = view as? UIImageView, view.bounds.height <= 1.0 {
+            return view
+        }
+        
+        for subview in view.subviews {
+            if let imageView = hairlineImageViewInNavigationBar(view: subview) {
+                return imageView
+            }
+        }
+        
+        return nil
+    }
+}
