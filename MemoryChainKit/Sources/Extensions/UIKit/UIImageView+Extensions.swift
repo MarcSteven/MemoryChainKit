6faed1935245faacd6cf8ca5f98a071d946ff1c0
Marc Steven <MarcSteven@users.noreply.github.com>
2020-12-29 22:53:16 +0800

    add method to calculate the height of UIImage

--- MemoryChainKit/Sources/Extensions/UIKit/UIImageView+Extensions.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIImageView+Extensions.swift
@@ -11,6 +11,24 @@
 import UIKit
 import ObjectiveC
 #if !os(watchOS)
+public extension UIImageView {
+    
+    
+        /*!
+         Calculates the height of the the UIImageView has to
+         have so the image is displayed correctly
+         - returns: Height to set on the imageView
+         */
+        func mc_heightForImageView() -> CGFloat {
+            guard let image = image, image.size.height > 0 else {
+                return 0.0
+            }
+            let width = bounds.size.width
+            let ratio = image.size.height / image.size.width
+            return width * ratio
+        }
+    
+}
 
 public extension UIImageView {
   func configureAppIconView(forImage appImage: UIImage, size: CGFloat) {
