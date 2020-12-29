fbe570a65ffe6e0443869950432ae60d34ff26c3
Marc Steven <MarcSteven@users.noreply.github.com>
2020-12-29 22:55:03 +0800

    add method for the UIViewController

--- MemoryChainKit/Sources/Extensions/UIKit/UIViewController+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIViewController+Extension.swift
@@ -8,7 +8,30 @@
 
 import UIKit
 
+// http://stackoverflow.com/questions/2777438/how-to-tell-if-uiviewcontrollers-view-is-visible
+public  extension UIViewController {
 
+    var isTopAndVisible: Bool {
+        return isVisible && isTopViewController
+    }
+
+    var isVisible: Bool {
+        if isViewLoaded {
+            return view.window != nil
+        }
+        return false
+    }
+
+    var isTopViewController: Bool {
+        if self.navigationController != nil {
+            return self.navigationController?.visibleViewController === self
+        } else if self.tabBarController != nil {
+            return self.tabBarController?.selectedViewController == self && self.presentedViewController == nil
+        } else {
+            return self.presentedViewController == nil && self.isVisible
+        }
+    }
+}
 public extension  UIViewController {
     func install(_ child:UIViewController) {
         addChild(child)
