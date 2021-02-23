3072193ab24df55086a496992c69dc2a3d4574a7
Marc Steven <MarcSteven@users.noreply.github.com>
2021-02-23 13:36:25 +0800

    add method to set alpha for UINavigationBar

--- MemoryChainKit/Sources/Extensions/UIKit/UINavigationBar+Style.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UINavigationBar+Style.swift
@@ -17,6 +17,49 @@ public extension UIBarItem {
 }
 
 public extension UINavigationBar {
+    
+    func setTitleAlpha(_ alpha: CGFloat) {
+        let color = titleTextAttributes?[.foregroundColor] as? UIColor ?? defaultTitleColor
+        setTitleColor(color.withAlphaComponent(alpha))
+    }
+    
+    @available(iOS 11.0, *)
+    func setLargeTitleAlpha(_ alpha: CGFloat) {
+        let color = largeTitleTextAttributes?[.foregroundColor] as? UIColor ?? defaultTitleColor
+        setLargeTitleColor(color.withAlphaComponent(alpha))
+    }
+    
+    func setTintAlpha(_ alpha: CGFloat) {
+        tintColor = tintColor.withAlphaComponent(alpha)
+    }
+}
+
+private extension UINavigationBar {
+    
+    var defaultTitleColor: UIColor {
+        return barStyle == .default ? UIColor.black : UIColor.white
+    }
+    
+    func setTitleColor(_ color: UIColor) {
+        if var titleTextAttributes = titleTextAttributes {
+            titleTextAttributes[.foregroundColor] = color
+            self.titleTextAttributes = titleTextAttributes
+        } else {
+            titleTextAttributes = [.foregroundColor: color]
+        }
+    }
+    
+    @available(iOS 11.0, *)
+    func setLargeTitleColor(_ color: UIColor) {
+        if var largeTitleTextAttributes = largeTitleTextAttributes {
+            largeTitleTextAttributes[.foregroundColor] = color
+            self.largeTitleTextAttributes = largeTitleTextAttributes
+        } else {
+            self.largeTitleTextAttributes = [.foregroundColor: color]
+        }
+    }
+}
+public extension UINavigationBar {
     func apply(_ style:Stylable) {
         applyColors(barColor:style.barColor , barTintColor: style.tintColor)
     }
