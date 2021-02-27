3af0fe0dcc111a1357d5805a658d43ce6ff9b105
Marc Steven <MarcSteven@users.noreply.github.com>
2021-02-27 12:26:38 +0800

    enlarge the area of button to touch for the button

--- MemoryChainKit/Sources/Extensions/UIKit/UIButton+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIButton+Extension.swift
@@ -20,6 +20,24 @@ public extension UIButton {
         return button
     }
 }
+fileprivate let minimumHitArea = CGSize(width: 44, height: 44)
+
+extension UIButton {
+    open override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        // if the button is hidden/disabled/transparent it can't be hit
+        if isHidden || !isUserInteractionEnabled || alpha < 0.01 { return nil }
+
+        // increase the hit frame to be at least as big as `minimumHitArea`
+        let buttonSize = bounds.size
+        let widthToAdd = max(minimumHitArea.width - buttonSize.width, 0)
+        let heightToAdd = max(minimumHitArea.height - buttonSize.height, 0)
+        let largerFrame = bounds.insetBy(dx: -widthToAdd / 2, dy: -heightToAdd / 2)
+
+        // perform hit test on larger frame
+        return (largerFrame.contains(point)) ? self : nil
+    }
+}
+
 public extension UIButton {
   
  static func createSystemButton(withTitle title: String)
