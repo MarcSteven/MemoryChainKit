f1d371e1117614e0f727b76de2b4c860f9750cb8
marczhao <zhaoxq@poweroak.net>
2021-08-30 15:59:55 +0800

    add method for UINavigationController to add bottom line

--- MemoryChainKit/Sources/Extensions/UIKit/UINavigationControllerExtension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UINavigationControllerExtension.swift
@@ -68,3 +68,31 @@ public     var rootViewController:UIViewController? {
     }
 
 }
+
+
+
+/MARK: - UINavigationController extension to add bottom line
+@objc public extension UINavigationController {
+    
+    
+    //MARK : - Add bottom line to the NavigationBarController
+    
+    
+        func addBottomLine(color:UIColor,height:Double)
+        {
+            //Hiding Default Line and Shadow
+            navigationBar.setValue(true, forKey: "hidesShadow")
+        
+            //Creating New line
+            let lineView = UIView(frame: CGRect(x: 0, y: 0, width:0, height: height))
+            lineView.backgroundColor = color
+            navigationBar.addSubview(lineView)
+        
+            lineView.translatesAutoresizingMaskIntoConstraints = false
+            lineView.widthAnchor.constraint(equalTo: navigationBar.widthAnchor).isActive = true
+            lineView.heightAnchor.constraint(equalToConstant: CGFloat(height)).isActive = true
+            lineView.centerXAnchor.constraint(equalTo: navigationBar.centerXAnchor).isActive = true
+            lineView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor).isActive = true
+        }
+    }
+
