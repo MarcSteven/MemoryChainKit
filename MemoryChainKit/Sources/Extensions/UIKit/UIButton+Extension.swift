07e108aea2b32eed4cd1b766b4d1a5e9b9a6cf56
Marc Steven <zhaoxinqiang328@gmail.com>
2021-12-22 22:11:23 +0800

    add Publisher extension

--- MemoryChainKit/Sources/Extensions/UIKit/UIButton+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIButton+Extension.swift
@@ -526,8 +526,54 @@ public extension UIButton {
        }
    }
     
+
+
 #endif
 //MARK: - build custom controls and live preview the design in the interface builder
 
 #endif
 
+
+public extension UIButton {
+  /**
+   The key to use for the localized button title in its normal state.
+   */
+  var normalLocalizedKey: String {
+    set(key) {
+      self.setTitle(localizedString(key: key, defaultValue: self.title(for: []) ?? ""), for: [])
+    }
+    get {
+      return ""
+    }
+  }
+
+  /**
+   The key to use for the localized button title in its selected state.
+   */
+  var selectedLocalizedKey: String {
+    set(key) {
+      self.setTitle(
+        localizedString(key: key, defaultValue: self.title(for: .selected) ?? ""),
+        for: .selected
+      )
+    }
+    get {
+      return ""
+    }
+  }
+
+  /**
+   The key to use for the localized button title in its disabled state.
+   */
+  var disabledLocalizedKey: String {
+    set(key) {
+      self.setTitle(
+        localizedString(key: key, defaultValue: self.title(for: .disabled) ?? ""),
+        for: .disabled
+      )
+    }
+    get {
+      return ""
+    }
+  }
+}
--- MemoryChainKit/Sources/Extensions/UIKit/UILabel+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UILabel+Extension.swift
@@ -104,3 +104,21 @@ public extension UILabel {
 }
 
 
+public extension UILabel {
+  /**
+   Turn this option on in IB to have the value of the label cleared on initialization.
+   */
+  var clearIBValue: Bool {
+    set(clear) {
+      #if !TARGET_INTERFACE_BUILDER
+        if clear {
+          self.text = ""
+        }
+      #endif
+    }
+    get {
+      return false
+    }
+  }
+}
+
