8d7e3462fa3db3e7bdda6661c71a754618f3a87e
Marc Steven <MarcSteven@users.noreply.github.com>
2023-02-07 19:23:49 +0800

    Delete AudioPlayerManager.swift

--- MemoryChainKit/Sources/UI Component/AudioPlayer/AudioPlayerManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AudioPlayerManager.swift
-//  MemoryChainKit
-//
-//  Created by MarcSteven on 2021/11/29.
-//  Copyright © 2021 Marc Steven(https://marcsteven.top). All rights reserved.
-//
-
-import Foundation
