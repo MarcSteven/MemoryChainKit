048b1a72f392dbf9ec4e9bd7f7efc0ee672c9ca5
Marc Steven <MarcSteven@users.noreply.github.com>
2023-10-18 17:56:33 +0800

    Update UIBarButtonItemExtension.swift
    make #selector to refer to a closure

--- MemoryChainKit/Sources/Extensions/UIKit/UIBarButtonItemExtension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIBarButtonItemExtension.swift
@@ -131,3 +131,38 @@ extension TargetActionBlockRepresentable where Self: UIBarButtonItem {
         setActionHandler(handler)
     }
 }
+
+
+
+
+
+
+private var actionKey: Void?
+
+public extension UIBarButtonItem {
+    // https://stackoverflow.com/a/36983811/9506784
+    private var _action: () -> Void {
+        get {
+            return objc_getAssociatedObject(self, &actionKey) as? () -> Void ?? { }
+        }
+        set {
+            objc_setAssociatedObject(self, &actionKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+
+    convenience init(title: String?, style: UIBarButtonItem.Style, action: @escaping () -> Void) {
+        self.init(title: title, style: style, target: nil, action: #selector(pressed))
+        self.target = self
+        self._action = action
+    }
+
+    convenience init(image: UIImage?, style: UIBarButtonItem.Style, action: @escaping () -> Void) {
+        self.init(image: image, style: style, target: nil, action: #selector(pressed))
+        self.target = self
+        self._action = action
+    }
+
+    @objc private func pressed(sender: UIBarButtonItem) {
+        _action()
+    }
+}
