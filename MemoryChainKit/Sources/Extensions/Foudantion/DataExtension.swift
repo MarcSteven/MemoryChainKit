a4b0206dc94664b026620a481a945a9e6ab5ed68
Marc Steven <MarcSteven@users.noreply.github.com>
2023-10-20 21:22:11 +0800

    add method to print json

--- MemoryChainKit/Sources/Extensions/Foudantion/DataExtension.swift
+++ MemoryChainKit/Sources/Extensions/Foudantion/DataExtension.swift
@@ -78,3 +78,37 @@ public extension Data {
         return nil
     }
 }
+
+
+
+public extension Data {
+
+    func printJson() {
+        do {
+            let json = try JSONSerialization.jsonObject(with: self, options: [])
+            let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+            guard let jsonString = String(data: data, encoding: .utf8) else {
+                print("Inavlid data")
+                return
+            }
+            print(jsonString)
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+
+    func jsonToString() -> String {
+        do {
+            let json = try JSONSerialization.jsonObject(with: self, options: [])
+            let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+            guard let jsonString = String(data: data, encoding: .utf8) else {
+                print("Inavlid data")
+                return ""
+            }
+            return jsonString
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
+        return ""
+    }
+}
