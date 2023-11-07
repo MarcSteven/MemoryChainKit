3398d58776ddefebc213324ec2011169c902003d
MarcSteven <137871653@qq.com>
2023-11-07 10:42:53 +0800

    add method to UIView/UIViewController

--- MemoryChainKit/Sources/Extensions/UIKit/UIViewController+Extension.swift
+++ MemoryChainKit/Sources/Extensions/UIKit/UIViewController+Extension.swift
@@ -324,3 +324,27 @@ public extension UIViewController {
         }
     } 
 }
+
+public extension UIViewController {
+    /// iOS's `isBeingDismissed` can return `false` if the VC is being dismissed indirectly, by one of its ancestors
+    /// being dismissed.  This method returns `true` if the VC is being dismissed directly, or if one of its ancestors is being
+    /// dismissed.
+    ///
+    func isBeingDismissedDirectlyOrByAncestor() -> Bool {
+        guard !isBeingDismissed else {
+            return true
+        }
+
+        var current: UIViewController = self
+
+        while let ancestor = current.parent {
+            guard !ancestor.isBeingDismissed else {
+                return true
+            }
+
+            current = ancestor
+        }
+
+        return false
+    }
+}
