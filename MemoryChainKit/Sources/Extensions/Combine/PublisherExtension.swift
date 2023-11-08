96a84ca030e3a92542f46990b3e606f67e0295c3
MarcSteven <137871653@qq.com>
2023-11-08 17:06:23 +0800

    add publisher extension

---
+++ MemoryChainKit/Sources/Extensions/Combine/PublisherExtension.swift
@@ -0,0 +1,50 @@
+//
+//  PublisherExtension.swift
+//  MemoryChainKit
+//
+//  Created by Marc Steven on 2023/11/8.
+//  Copyright © 2023 Marc Steven(https://marcsteven.top). All rights reserved.
+//
+
+import Foundation
+import Combine
+
+
+@available(iOS 13.0, *)
+extension Publisher where Failure == Never {
+    
+    public func sink<Root>(to handler: @escaping (Root) -> (Output) -> Void, on root: Root) -> AnyCancellable where Root: AnyObject {
+        sink{ [weak root] value in
+            guard let root = root else { return }
+            handler(root)(value)
+        }
+    }
+    
+    public func sink<Root>(to handler: @escaping (Root) -> () -> Void, on root: Root) -> AnyCancellable where Root: AnyObject {
+        sink{ [weak root] _ in
+            guard let root = root else { return }
+            handler(root)()
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+extension Publisher {
+    
+    public func eraseToAnyError() -> Publishers.MapError<Self, Error> {
+        mapError { $0 as Error }
+    }
+    
+    public func `catch`(_ handler: @escaping (Failure) -> Output) -> Publishers.Catch<Self, Just<Output>> {
+        `catch`{ error in Just(handler(error)) }
+    }
+    
+    public func flatMap<P>(
+        maxPublishers: Subscribers.Demand = .unlimited,
+        _ transform: @escaping (Output) -> P
+    ) -> Publishers.FlatMap<Publishers.MapError<P, Error>, Publishers.MapError<Self, Error>> where P: Publisher {
+        eraseToAnyError().flatMap { output in
+            transform(output).eraseToAnyError()
+        }
+    }
+}
